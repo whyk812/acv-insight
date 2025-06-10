2e0eedcc695a71c0f0a49433d63f64c0864cc5a6
whyk812 <jjwh0812@naver.com>
2025-06-10 12:15:58 +0900

    feat: CSV 분석 함수 구현

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,2 +1,10 @@
 from pathlib import Path
 import pandas as pd
+
+def compute_stats(df: pd.DataFrame) -> dict:
+    """
+    DataFrame의 각 숫자형 컬럼에 대해
+    평균, 표준편차, 최솟값, 최댓값을 계산하여
+    {컬럼명: { 'mean':…, 'std':…, 'min':…, 'max':… }, …} 형태로 반환합니다.
+    """
+    
